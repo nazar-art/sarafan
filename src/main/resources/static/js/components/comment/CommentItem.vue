8cd48f956b55f7f51a3c1818acec51cb434cdd19
Nazar lelyak <nazar.lelyak@globallogic.com>
2020-10-28 20:07:18 +0200

    fix issues with CommentItem.vue

--- src/main/resources/static/js/components/comment/CommentItem.vue
+++ src/main/resources/static/js/components/comment/CommentItem.vue
@@ -1,26 +1,13 @@
-<!--<template>
-    <v-list-tile>
-        <v-list-tile-avatar>
-            <img :src="comment.author.userpic">
-        </v-list-tile-avatar>
-
-        <v-list-tile-content>
-            <v-list-tile-title>{{comment.text}}</v-list-tile-title>
-            <v-list-tile-sub-title>{{comment.author.name}}</v-list-tile-sub-title>
-        </v-list-tile-content>
-    </v-list-tile>
-</template>-->
-
 <template>
-    <v-list-item>
+    <v-list-tile>
         <user-link
                 :user="comment.author"
         ></user-link>
 
-        <v-list-item-content>
-            <v-list-item-title>{{comment.text}}</v-list-item-title>
-        </v-list-item-content>
-    </v-list-item>
+        <v-list-tile-content>
+            <v-list-tile-title>{{comment.text}}</v-list-tile-title>
+        </v-list-tile-content>
+    </v-list-tile>
 </template>
 
 <script>
