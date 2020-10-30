d0f05f4bf754d5f0658f730c4f3489ee39a37b22
Nazar lelyak <nazar.lelyak@globallogic.com>
2020-10-30 16:22:19 +0200

    add building jar for included UI

--- src/main/java/edu/lelyak/controller/MainController.java
+++ src/main/java/edu/lelyak/controller/MainController.java
@@ -28,7 +28,7 @@ public class MainController {
     private final MessageService messageService;
     private final UserDetailsRepository userDetailsRepo;
 
-    @Value("${spring.profiles.active}")
+    @Value("${spring.profiles.active:prod}")
     private String profile;
 
     private final ObjectWriter messageWriter;
